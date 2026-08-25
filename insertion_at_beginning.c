835f74f6f8b73459d506aad18beaba9dbf973547
Kritima <kritimatomar@gmail.com>
2026-08-25 12:07:30 +0530

    Add files via upload

---
+++ insertion_at_beginning.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head, *new, *temp;
+int main()
+{
+    int ch;
+    do
+    {
+        new = (struct node *)malloc(sizeof(struct node));
+        printf("Enter the element to be inserted: ");
+        scanf("%d", &new->data);
+        new->next = NULL;
+        if (head == NULL)
+        {
+            head = new;
+            temp = new;
+        }
+        else
+        {
+
+            temp->next = new;
+            temp = temp->next;
+        }
+        printf("Do you want to continue? (1 for yes, 0 for no): ");
+        scanf("%d", &ch);
+    } while (ch == 1);
+    new = (struct node *)malloc(sizeof(struct node));
+    printf("enter the element to be inserted at the beginning: ");
+    scanf("%d", &new->data);
+    
+    new->next = head;
+    head = new;
+    struct node *a;
+    a = head;
+    printf("The elements in the linked list are: ");
+    while (a != NULL)
+    {
+        printf("%d ", a->data);
+        a = a->next;
+    }
+    return 0;
+}
